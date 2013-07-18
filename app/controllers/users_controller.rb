40ae93198906b6ac86cf2481433780099509580e
Matthew Looi <looi.matthew1@gmail.com>
2013-07-18 11:33:54 +0200

    here

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  respond_to :html, :json
   # GET /users
   # GET /users.json
+
   def index
     @users = User.all
+    respond_with @users
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+     @user = User.find(params[:id])
+    respond_with @users
   end
 
   # GET /users/new
@@ -19,46 +23,31 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+     @user = User.find(params[:id])
   end
 
   # POST /users
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user.save
+    respond_with @users
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+     @user = User.find(params[:id])
+     @user.update_attributes(params[ :user])
+     respond_with @users
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    respond_with @users
   end
 
   private
